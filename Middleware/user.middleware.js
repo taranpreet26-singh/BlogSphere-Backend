d0dc0b4b0037831e9d10cd44f86fd8ea34fc03eb
Taranpreet Singh <ramghrialoveysingh123@gmail.com>
2025-05-05 00:46:42 +0530

    fix issue

--- Middleware/user.middleware.js
+++ Middleware/user.middleware.js
@@ -7,6 +7,7 @@ export async function userMiddleware(req,res,next) {
     console.log(token)
     
     const response = jwt.verify(token,process.env.SIGNATURE)
+    console.log(response)
     const userExist = await prisma.user.findFirst({
         where:{
             id:response.id
